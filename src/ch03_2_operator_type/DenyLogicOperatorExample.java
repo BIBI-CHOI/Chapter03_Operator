b0a408f4f367d2dbf2710a826d1ac3f3a97f211c
User <User@DESKTOP-20GJ8HB>
2021-08-05 01:14:36 +0900

    21.08.05(1)

---
+++ src/ch03_2_operator_type/DenyLogicOperatorExample.java
@@ -0,0 +1,17 @@
+package ch03_2_operator_type;
+
+public class DenyLogicOperatorExample {
+
+	public static void main(String[] args) {
+		boolean play = true;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+		
+		play = !play;
+		System.out.println(play);
+
+	}
+
+}
